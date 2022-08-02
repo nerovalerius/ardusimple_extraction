61211fd7ea287f6425faec11a8f7fd684df1aca7
nero <nero@DESKTOP-02KUUMQ.localdomain>
2022-08-02 11:05:17 +0200

    first version finished

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ardusimple_extraction
 Extracts Location and Timestamp from the ardusimple ROS topic
+
+## How to interpret the values
+http://docs.ros.org/en/lunar/api/sensor_msgs/html/msg/NavSatStatus.html
+http://docs.ros.org/en/lunar/api/sensor_msgs/html/msg/NavSatFix.html
+
+## How to run
+Play rosbag:
+```ros2 bag play rosbag2_2022_07_05-11_56_57_0.db3 -s sqlite3```
+
+Run node:
+```ros2 run ros_topic_extraction ardusimple_extraction```
--- ros_topic_extraction/ardusimple_extraction.py
+++ ros_topic_extraction/ardusimple_extraction.py
@@ -1,5 +1,51 @@
-def main():
-    print('Hi from ros_topic_extraction.')
+import rclpy
+import csv
+import datetime
+
+from rclpy.node import Node
+from sensor_msgs.msg import NavSatFix
+
+class MinimalSubscriber(Node):
+
+    writer = None 
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+
+        # Initialize CSV Writer and Header
+        date = datetime.datetime.now()
+        filename = "ardusimple_" + str(date.day) + "-" + str(date.month) + "-" + str(date.year) + "-" + str(date.hour) + "-" + str(date.minute) + ".csv"
+        self.writer = csv.writer(open(filename, "w"), delimiter=";")
+        csv_header = ['timestamp sec', 'timestamp nanosec', 'latitude', 'longitude', 'status']
+        self.writer.writerow(csv_header)
+
+        # Subscribe to topic
+        self.subscription = self.create_subscription(
+            NavSatFix,
+            'navsat_topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        #self.get_logger().info('lat: "%s"' % msg.latitude)
+        csv_data = [msg.header.stamp.sec, msg.header.stamp.nanosec, msg.latitude, msg.longitude, msg.status.status]
+        self.writer.writerow(csv_data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
